dde05003212a09eb1c71297112fc074e238bbe86
tiaggomoura <tiago.rodrigues.dev@gmail.com>
2021-07-22 00:05:06 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/entities/Worker.java
@@ -0,0 +1,18 @@
+package com.microservice.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Worker {
+
+	private Long id;
+	private String name;
+	private Double dailyIncome;
+
+}
--- hr-payroll/src/main/java/com/microservice/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/service/PaymentService.java
@@ -1,17 +1,32 @@
 package com.microservice.hrpayroll.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.microservice.hrpayroll.entities.Payment;
+import com.microservice.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 	public Payment getPayment(long workerId, int days) {
-		return Payment.builder()
-				.name("Bob")
-				.days(days)
-				.dailyIncome(200.0)
-				.build();
+
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", "" + workerId);
+
+		Worker worker = this.restTemplate.getForObject(this.workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+		return Payment.builder().name(worker.getName()).days(days).dailyIncome(worker.getDailyIncome()).build();
 	}
 }
