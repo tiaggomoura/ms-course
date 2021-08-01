5d9223c66e4afddf163748e13722b63f216591a6
tiaggomoura <tiago.rodrigues.dev@gmail.com>
2021-08-01 09:39:48 -0300

    Created Config server project

---
+++ hr-config-server/src/main/java/com/microservice/hrconfigserver/HrApiConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiConfigServerApplication.class, args);
+	}
+
+}
