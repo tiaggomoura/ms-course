2f416c0eca5d95f9746529a9aa992fb56f5d62cc
tiaggomoura <tiago.rodrigues.dev@gmail.com>
2021-08-02 13:36:02 -0300

    Config cliente server and config Actuator Refresh in Runtime

--- hr-worker/src/main/java/com/microservice/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/microservice/hrworker/resources/WorkerResource.java
@@ -2,7 +2,11 @@ package com.microservice.hrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
 import com.microservice.hrworker.entities.Worker;
 import com.microservice.hrworker.services.WorkerService;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+	@Value("${test.config}")
+	private String testConfig;
+
 	@Autowired
 	private WorkerService service;
+
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
