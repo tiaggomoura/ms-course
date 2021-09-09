5e1c185c3ac4004a3f820eb5b911214ce1946c9d
tiaggomoura <tiago.rodrigues.dev@gmail.com>
2021-09-08 23:56:20 -0300

    UserRepository,UserResource and ZuulConfig

---
+++ hr-user/src/main/java/com/microservice/hruser/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.microservice.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.microservice.hruser.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+}
